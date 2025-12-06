6c5125f03be39627760799dbf2f73a9b0b79d8d0
olympia15 <olympiamichelle15@gmail.com>
2025-12-05 19:49:37 -0500

    input validation functions created in MainScreen.jsx

--- screens/MainScreen.jsx
+++ screens/MainScreen.jsx
@@ -9,7 +9,44 @@ export default function MainScreen({ navigation }) {
     const [amount, setAmount] = useState("1");
     const [loading, setLoading] = useState(false);
 
-    
+    // validate currency code format
+    const validateCurrencyCode = (code) => {
+        const regex = /^[A-Z]{3}$/; // exactly 3 uppercase letters
+        return regex.test(code);
+    }
+
+    // validate amount (must be positive)
+    const validateAmount = (value) => {
+        const number = parseFloat(value); // convert string to number (float)
+        return !isNaN(number) && number > 0; // check if number is valid and positive
+    }
+
+    // convert currency function
+    const convertCurrency = async () => {
+
+        // validate currency input
+        if (!validateCurrencyCode(baseCurrency)) {
+            Alert.alert("Invalid Input", "Base currency must be a 3 letter code (e.g., USD, CAD).");
+            return;
+        }
+
+        // validate destination amount input
+        if (!validateCurrencyCode(destinationCurrency)) {
+            Alert.alert("Invalid Input", "Destination currency must be a 3 letter code (e.g., USD, CAD).");
+        }
+
+        // validate amount input
+        if (!validateAmount(amount)) {
+            Alert.alert("Invalid Input", "Amount must be a positive number.");
+            return;
+        }
+
+        setLoading(true);
+        setResult(null); 
+
+    }
+
+
 
 
 
